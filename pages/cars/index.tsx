109f3275fb387880540c5684d9397b7a09a4b5fa
David Odoh <david.odoh1@gmail.com>
2022-07-06 15:01:57 +0100

    fix

--- pages/cars/index.tsx
+++ pages/cars/index.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 import Link from 'next/link'
 import Head from 'next/head'
-import Pagination from '@etchteam/next-pagination'
+// import Pagination from '@etchteam/next-pagination'
 
 import styles from "../../styles/Home.module.scss"
 import styles3 from "../../styles/components/pagination.module.scss"
